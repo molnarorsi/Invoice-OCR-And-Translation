ebe038dec63f6419962a47dd0d5ff308437e63a2
molnarorsi <79136111+molnarorsi@users.noreply.github.com>
2024-03-19 23:12:23 +0200

    added opencv.js and display document

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -14,12 +14,12 @@ const Navbar = () => {
         <>
             <div className={classes.rootContainer}>
                 <Grid container spacing={2} sx={{my: "auto"}}>
-                    <Grid item xs={2}></Grid>
-                        <Grid item xs={8} sx={{mt: 1, textAlign:"center"}}>
+                    <Grid item xs={2}>
+                    <Grid item xs={8} sx={{mt: 1, textAlign:"center"}}>
                             <Typography variant="h4">Welcome Back!!</Typography>
                         </Grid>
+                    </Grid>
                         
-                    
                     <Grid item xs={2} sx={{mt: 1, textAlign: "right"}}>
                         <Button
                             className={classes.logout}
--- frontend/src/components/SelectCard/SelectCard.js
+++ frontend/src/components/SelectCard/SelectCard.js
@@ -1,8 +1,19 @@
 import { Button, Typography } from "@mui/material";
 import { useStyles } from "./styles";
+const cv = window.cv;
 
 const SelectCard = () => {
     const classes = useStyles();
+
+    const handleImageUpload = (event) => {
+        const img = new Image();
+        img.onload = () => {
+            const mat = cv.imread(img);
+            cv.imshow("output", mat);
+            mat.delete();
+        };
+        img.src = URL.createObjectURL(event.target.files[0]);
+    };
 
     return (
         <>
@@ -14,9 +25,10 @@ const SelectCard = () => {
                 <input
                     type="file"
                     className="form-control"
+                    onChange={(e) => handleImageUpload(e)}
                 />
             </div>
-            <Button variant="contained" sx={{margin: "20px", px: "50px"}}>
+            <Button variant="contained" sx={{margin: "20px", px: "10%"}}>
                 Next
             </Button>
         </div>
