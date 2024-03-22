5a6776d999baa0ad14df644e3575602a58ee9d19
molnarorsi <79136111+molnarorsi@users.noreply.github.com>
2024-03-22 23:45:09 +0200

    dockerized opencv

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_cors import CORS, cross_origin
+from preprocessing import preprocessing_bp
 
 app = Flask(__name__)
-cors = CORS(app)
+
+app.register_blueprint(preprocessing_bp)
+cors = CORS(app, resources={r"/grayscale": {"origins": "http://localhost:3000"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/hello")
