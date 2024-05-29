8ae3942c2daf29cb96f446814a01518ea0104423
molnarorsi <79136111+molnarorsi@users.noreply.github.com>
2024-05-29 21:25:43 +0300

    added invoices to database

--- backend/app/models.py
+++ backend/app/models.py
@@ -20,3 +20,17 @@ class PDFSource(db.Model):
     source_id = db.Column(db.String(120), unique=True, nullable=False)
     user_id = db.Column(db.String(32), db.ForeignKey('users.id'), nullable=False) 
     
+
+class Invoice(db.Model):
+    __tablename__ = "invoices"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    invoice_number = db.Column(db.String(100))
+    cif = db.Column(db.String(100))
+    date_of_issue = db.Column(db.String(100))
+    due_date = db.Column(db.String(100))
+    total_price = db.Column(db.String(100))
+    IBAN = db.Column(db.String(100))
+    bank = db.Column(db.String(100))
+    buyer_CIF = db.Column(db.String(100))
+    supplier_CIF = db.Column(db.String(100))
+    
--- backend/app/tesseractOCR.py
+++ backend/app/tesseractOCR.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 import pytesseract
 from app.parserOCR import parse_text
+from app.models import db, Invoice
 
 tesseract_bp = Blueprint('tesseract', __name__)
 
@@ -13,9 +14,16 @@ def load_image():
     img = cv2.cvtColor(img , cv2.COLOR_BGR2RGB)
     return img
 
+
+def add_invoice(parsed_text):
+    invoice = Invoice(**parsed_text)
+    db.session.add(invoice)
+    db.session.commit()
+
 @tesseract_bp.route('/tesseract', methods=['POST'])
 def tesseract():
     img = load_image()
     text = pytesseract.image_to_string(img)
     parsed_text = parse_text(text)
+    add_invoice(parsed_text)
     return jsonify({'text': text, 'parsed_text': parsed_text})
